453682b64964aa17a3eec3183ce5646f35a4c2a4
Mark Schenk <m.m.a.schenk@tudelft.nl>
2023-01-19 14:15:28 +0100

    Working scanner

--- runrtl.sh
+++ runrtl.sh
@@ -2,14 +2,22 @@
 echo "MQTT_URL = ${MQTT_URL}"
 echo "MQTT_USER = ${MQTT_USER}"
 echo "MQTT_PASSWORD = ${MQTT_PASSWORD}"
+echo "MQTT_RTL433_EVENT_QUEUE" = ${MQTT_RTL433_EVENT_QUEUE}
 
-FSTRING="${MQTT_URL},user=${MQTT_USER},pass=${MQTT_PASSWORD}"
+FSTRING="${MQTT_URL},user=${MQTT_USER},pass=${MQTT_PASSWORD},events=${MQTT_RTL433_EVENT_QUEUE}"
 
 echo "FSTRING = ${FSTRING}"
 
-echo rtl_433 -v -M time:tz \
+echo Running: rtl_433 -v -M time:tz \
     -M protocol \
     -M level \
     -C si \
-    -F ${FSTRING} \
+    -F "${FSTRING}" \
+    -F kv
+
+rtl_433 -v -M time:tz \
+    -M protocol \
+    -M level \
+    -C si \
+    -F "${FSTRING}" \
     -F kv
